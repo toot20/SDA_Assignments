89d4602d5bc54bd40c23935afa0c1e52b4a6c19f
toot20 <atikahalmutlq.CS@gmail.com>
2024-03-13 21:49:09 +0300

    screenshot Test

--- src/test/java/week5_Assignments/TaskSixeTest.java
+++ src/test/java/week5_Assignments/TaskSixeTest.java
@@ -12,17 +12,16 @@ import java.io.IOException;
 //import java.nio.file.Files;
 
 public class TaskSixeTest extends Tests{
-    // Go to amazon.com
-    // Take Full Page Screenshot.
-    // Take any spesific WebElement ScreenShot
     @Test
     public void ScreenShotTest() throws IOException {
+        // Go to ebay.com
         bot.navigate("https://www.ebay.com/");
 
-        // Take a screenshot of the entire page
+        // Take Full Page Screenshot.
         File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         Files.copy(screenshot, new File("target/screenshot_"+System.currentTimeMillis()+".png"));
 
+        // Take any spesific WebElement ScreenShot
         By ebayImage = By.id("gh-la");
         File ElementScreenshot = driver.findElement(ebayImage).getScreenshotAs(OutputType.FILE);
         Files.copy(ElementScreenshot, new File("target/ElementScreenshot_"+System.currentTimeMillis()+".png"));
