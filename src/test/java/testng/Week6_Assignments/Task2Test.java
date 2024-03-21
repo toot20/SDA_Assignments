a61449a09bf87a61416952b2683b5f8dcb92f9ef
toot20 <atikahalmutlq.CS@gmail.com>
2024-03-22 02:43:14 +0300

    Excel Data Provider Test

--- src/test/java/testng/Week6_Assignments/Task2Test.java
+++ src/test/java/testng/Week6_Assignments/Task2Test.java
@@ -42,23 +42,6 @@ Run it parallel with 3 threads
 
           return data;
       }
-     // public static Object[][] readDataFromExcel(String filePath, String sheetName) throws IOException {
-       //   FileInputStream fileInputStream = new FileInputStream(filePath);
-         // Workbook workbook = WorkbookFactory.create(fileInputStream);
-         // Sheet sheet = workbook.getSheet(sheetName);
-
-         // int rowCount = sheet.getPhysicalNumberOfRows();
-         // int colCount = sheet.getRow(0).getLastCellNum();
-
-          //Object[][] data = new Object[rowCount - 1][colCount];
-
-          //for (int i = 1; i < rowCount; i++) {
-          //    Row row = sheet.getRow(i);
-            //  for (int j = 0; j < colCount; j++) {
-             //     Cell cell = row.getCell(j);
-               //   data[i - 1][j] = cell.getStringCellValue();
-             // }
-          //}
 
     @DataProvider(name = "excelData")
     public Object[][] testData() throws IOException {
