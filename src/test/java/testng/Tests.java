7fb85e59f2a544da4f5fbb737a90abddb681c614
toot20 <atikahalmutlq.CS@gmail.com>
2024-03-27 21:08:12 +0300

    test base

--- src/test/java/testng/Tests.java
+++ src/test/java/testng/Tests.java
@@ -1,6 +1,8 @@
 package testng;
 
 import engine.ActionsBot;
+import engine.PropertiesReader;
+import io.qameta.allure.Step;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
@@ -11,6 +13,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -28,39 +31,45 @@ public abstract class Tests {
         protected static Logger logger;
         protected ActionsBot bot;
         protected static JSONObject testData;
+        protected JSONObject testCaseData;
 
-        @BeforeClass
-        public void beforeClass() throws IOException, ParseException {
+    @Step("Initializing test data and properties")
+    @BeforeClass
+    public static void globalSetup() throws IOException, ParseException {
+        Configurator.initialize(null, "src/main/resources/properties/log4j2.properties");
+        logger = LogManager.getLogger(Tests.class.getName());
+        testData =  (JSONObject) new JSONParser().parse( new FileReader("src/test/resources/testData/sample.json", StandardCharsets.UTF_8) );
+        PropertiesReader.readPropertyFile("src/main/resources/properties/configuration.properties");
+    }
 
-            Configurator.initialize(null, "src/main/resources/properties/log4j2.properties");
-            logger = LogManager.getLogger(testng.Tests.class.getName());
-            testData =  (JSONObject) new JSONParser().parse( new FileReader("src/test/resources/testData/sample.json", StandardCharsets.UTF_8) );
+    @Step("Initializing target browser")
+    @Parameters({ "target-browser" })
+    @BeforeMethod
+    public void browserInitialization(@Optional("chrome") String targetBrowser){
+        targetBrowser = PropertiesReader.props.getProperty("targetBrowser");
+        logger.info("Launching "+targetBrowser+" browser");
+
+        switch (targetBrowser){
+            case "chrome" -> driver = new ChromeDriver();
+            case "firefox" -> driver = new FirefoxDriver();
+            case "safari" -> driver = new SafariDriver();
+            case "edge" -> driver = new EdgeDriver();
         }
 
-        @Parameters({ "target-browser" })
-        @BeforeMethod
-        public void beforeMethod(@Optional("chrome") String targetBrowser){
-            logger.info("Opening "+targetBrowser+" Browser");
+       // driver = new EventFiringDecorator(new CustomListener()).decorate(driver);
 
-            switch (targetBrowser){
-                case "chrome" -> driver = new ChromeDriver();
-                case "firefox" -> driver = new FirefoxDriver();
-                case "edge" -> driver = new EdgeDriver();
-            }
+        driver.manage().window().maximize();
 
+        logger.info("Configuring 5 second explicit wait");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        bot = new ActionsBot(driver, wait, logger);
+    }
 
-
-            driver.manage().window().maximize();
-
-            logger.info("Configuring 5 second explicit wait");
-            wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-            bot = new ActionsBot(driver, wait, logger);
-        }
-
-        @AfterMethod
-        public void afterMethod(){
-            logger.info("Quitting Browser");
-            driver.quit();
-        }
+    @Step("Terminating target browser")
+    @AfterMethod
+    public void browserTermination(){
+        logger.info("Quitting Browser");
+        driver.quit();
+    }
     }
 
